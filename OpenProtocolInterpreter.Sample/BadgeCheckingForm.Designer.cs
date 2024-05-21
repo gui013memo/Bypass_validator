125527f1a5607622b66bade8808eabc8fd78c40d
Guilherme Oliveira <guilhermeoliveira36023@gmail.com>
2024-05-21 14:00:52 -0300

    First changes on CheckBadgeForm

--- OpenProtocolInterpreter.Sample/BadgeCheckingForm.Designer.cs
+++ OpenProtocolInterpreter.Sample/BadgeCheckingForm.Designer.cs
@@ -39,9 +39,10 @@
             this.bypassRequestedLabel.AutoSize = true;
             this.bypassRequestedLabel.Font = new System.Drawing.Font("Consolas", 60F, System.Drawing.FontStyle.Bold);
             this.bypassRequestedLabel.ForeColor = System.Drawing.Color.Blue;
-            this.bypassRequestedLabel.Location = new System.Drawing.Point(39, 9);
+            this.bypassRequestedLabel.Location = new System.Drawing.Point(29, 7);
+            this.bypassRequestedLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bypassRequestedLabel.Name = "bypassRequestedLabel";
-            this.bypassRequestedLabel.Size = new System.Drawing.Size(930, 117);
+            this.bypassRequestedLabel.Size = new System.Drawing.Size(744, 94);
             this.bypassRequestedLabel.TabIndex = 0;
             this.bypassRequestedLabel.Text = "BYPASS REQUESTED";
             // 
@@ -51,9 +52,10 @@
             this.operatorStatusLabel.BackColor = System.Drawing.Color.Yellow;
             this.operatorStatusLabel.Font = new System.Drawing.Font("Consolas", 38F);
             this.operatorStatusLabel.ForeColor = System.Drawing.Color.Black;
-            this.operatorStatusLabel.Location = new System.Drawing.Point(63, 126);
+            this.operatorStatusLabel.Location = new System.Drawing.Point(47, 102);
+            this.operatorStatusLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.operatorStatusLabel.Name = "operatorStatusLabel";
-            this.operatorStatusLabel.Size = new System.Drawing.Size(872, 75);
+            this.operatorStatusLabel.Size = new System.Drawing.Size(697, 60);
             this.operatorStatusLabel.TabIndex = 1;
             this.operatorStatusLabel.Text = "OPERADOR NÃO AUTORIZADO:";
             // 
@@ -61,9 +63,10 @@
             // 
             this.operatorNameLabel.AutoSize = true;
             this.operatorNameLabel.Font = new System.Drawing.Font("Consolas", 40F);
-            this.operatorNameLabel.Location = new System.Drawing.Point(129, 214);
+            this.operatorNameLabel.Location = new System.Drawing.Point(97, 174);
+            this.operatorNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.operatorNameLabel.Name = "operatorNameLabel";
-            this.operatorNameLabel.Size = new System.Drawing.Size(700, 79);
+            this.operatorNameLabel.Size = new System.Drawing.Size(567, 64);
             this.operatorNameLabel.TabIndex = 2;
             this.operatorNameLabel.Text = "Guilherme Oliveira";
             // 
@@ -71,23 +74,26 @@
             // 
             this.OperatorIdLabel.AutoSize = true;
             this.OperatorIdLabel.Font = new System.Drawing.Font("Consolas", 38F);
-            this.OperatorIdLabel.Location = new System.Drawing.Point(210, 293);
+            this.OperatorIdLabel.Location = new System.Drawing.Point(158, 238);
+            this.OperatorIdLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.OperatorIdLabel.Name = "OperatorIdLabel";
-            this.OperatorIdLabel.Size = new System.Drawing.Size(522, 75);
+            this.OperatorIdLabel.Size = new System.Drawing.Size(417, 60);
             this.OperatorIdLabel.TabIndex = 3;
             this.OperatorIdLabel.Text = "ID: 2406292014";
             // 
             // BadgeCheckingForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.ClientSize = new System.Drawing.Size(1044, 383);
+            this.ClientSize = new System.Drawing.Size(783, 311);
             this.Controls.Add(this.OperatorIdLabel);
             this.Controls.Add(this.operatorNameLabel);
             this.Controls.Add(this.operatorStatusLabel);
             this.Controls.Add(this.bypassRequestedLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Location = new System.Drawing.Point(350, 500);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "BadgeCheckingForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "BadgeCheckingForm";
