5d1cbc76c15222dfeae9f37482b994aa1a127694
Guilherme Oliveira <guilhermeoliveira36023@gmail.com>
2024-06-09 01:35:27 -0300

    Updating repo with SplashScreenForm items

---
+++ OpenProtocolInterpreter.Sample/SplashScreenForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace OpenProtocolInterpreter.Sample
+{
+    partial class SplashScreenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ShadeEffectTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(381, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1.0.0 - Release candidate";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::OpenProtocolInterpreter.Sample.Properties.Resources.icon;
+            this.pictureBox1.Location = new System.Drawing.Point(59, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(265, 208);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ShadeEffectTimer
+            // 
+            this.ShadeEffectTimer.Interval = 50;
+            this.ShadeEffectTimer.Tick += new System.EventHandler(this.ShadeEffectTimer_Tick);
+            // 
+            // SplashScreenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(393, 270);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreenForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplashScreenForm";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Timer ShadeEffectTimer;
+    }
+}
---
+++ OpenProtocolInterpreter.Sample/SplashScreenForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenProtocolInterpreter.Sample
+{
+    public partial class SplashScreenForm : Form
+    {
+        private ManualResetEvent splashCompletedEvent;
+
+        public SplashScreenForm(ManualResetEvent splashCompletedEvent)
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(SetRoundedRegion);
+            this.Opacity = 0;
+            this.splashCompletedEvent = splashCompletedEvent;
+        }
+
+        private void SetRoundedRegion(object sender, PaintEventArgs e)
+        {
+            int radius = 20;
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(new Rectangle(0, 0, radius, radius), 180, 90);
+            path.AddArc(new Rectangle(this.Width - radius, 0, radius, radius), -90, 90);
+            path.AddArc(new Rectangle(this.Width - radius, this.Height - radius, radius, radius), 0, 90);
+            path.AddArc(new Rectangle(0, this.Height - radius, radius, radius), 90, 90);
+            path.CloseFigure();
+            this.Region = new Region(path);
+        }
+
+        private void ShadeEffectTimer_Tick(object sender, EventArgs e)
+        {
+            if (this.Opacity < 1)
+                this.Opacity += 0.10;
+            else
+            {
+                ShadeEffectTimer.Stop();
+                Task.Delay(1000).ContinueWith(t => CloseSplashScreen());
+            }
+        }
+
+        public void StartShadeEffect()
+        {
+            ShadeEffectTimer.Start();
+        }
+
+        private void CloseSplashScreen()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(CloseSplashScreen));
+                return;
+            }
+
+            splashCompletedEvent.Set(); // Signal that the splash screen has completed
+            Close();
+        }
+    }
+}
